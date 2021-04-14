b1840b5a697bfbaecce71e429da0f288740eac74
xkhanhan <1427742566@qq.com>
2021-04-14 11:51:30 +0800

    添加：添加全局组件 control

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,25 @@
+/**
+ * 框架入口文件
+ */
 import xkCollapse from './collapse/index';
 import xkCollapseItem from './collapseItem/index';
 import xkProgress from './progress/index';
 import xkVideo from './video/index';
+import xkControl from './control/index';
 
 const components = [
     xkCollapse,
     xkCollapseItem,
     xkProgress,
-    xkVideo
+    xkVideo,
+    xkControl
 ]
 
+/**
+ * 全局注册
+ * @param {*} Vue 
+ * @param {*} option 
+ */
 const install = (Vue, option = {}) => {
     console.log(option);
     components.forEach(component => {
