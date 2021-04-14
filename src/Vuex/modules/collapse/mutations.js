d6904fee76c2c2a8494092614e90a277b004e9ae
xkhanhan <1427742566@qq.com>
2021-04-14 11:49:46 +0800

    添加：提交 activeList 的 commit 时对state做出不同情况下的处理

--- src/Vuex/modules/collapse/mutations.js
+++ src/Vuex/modules/collapse/mutations.js
@@ -1,9 +1,23 @@
 export default {
-    activeList(state, value) {
-        console.log(value);
-        state.activeList = value;
+    activeList(state, list) {
+        state.activeList = list;
+    },
+    activeValue(state, value) {
+        if (state.accordion) { // 手风琴模式
+            state.activeList.shift();
+            state.activeList.push(value);
+        } else {
+            const index = state.activeList.indexOf(value);
+
+            if (index == -1) { // 不存在，展示
+                state.activeList.push(value);
+            } else { // 已存在，再次点击即为折叠
+                state.activeList.splice(index, 1);
+            }
+        }
     },
     accordion(state, value) {
         state.accordion = value;
     }
+
 }
