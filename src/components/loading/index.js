1def0dc275033691c042f14a7c42ba7692d361b4
xkhanhan <1427742566@qq.com>
2021-04-16 16:07:57 +0800

    增加：增加loading组件

---
+++ src/components/loading/index.js
@@ -0,0 +1,7 @@
+import xkLoading from './loading.vue';
+
+xkLoading.install = (Vue) => {
+    Vue.component(xkLoading.name, xkLoading);
+}
+
+export default xkLoading;
---
+++ src/components/loading/loading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      loading
+  </div>
+</template>
+
+<script>
+export default {
+    name : "xkLoading"
+}
+</script>
+
+<style>
+
+</style>
